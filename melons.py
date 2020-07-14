9e44a1dd0609a4a045c25a2b5bc4b594b8341130
Anne Woo-Sam <annewoosam@gmail.com>
2020-07-14 01:16:32 +0000

    Final files.

--- melons.py
+++ melons.py
@@ -1,23 +1,55 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+# dictionary name melons
+# for each melon_name track the following characteristics
+# melon_price s/b price
+# melon_seedlessness s/b seedless
+# add flesh-color
+# add weight
+# add rind color
+# defaults should be 
+# seedless False
+# price 0.99
+# flesh-color,weight and rind-color None
+# move all existing data to lump together by key
+# note for each dictionary item there is no comma for the last value as nothing more needs to be separated. There is a comma between each dictionary item.
+melons={
+'Honeydew': {
+    'price': 0.99,
+    'seedless': True,
+    'flesh-color': None,
+    'weight': None,
+    'rind-color': None,
+    'locale': None
+    },
+'Crenshaw': {
+    'price': 2.00,
+    'seedless': False,
+    'flesh-color': None,
+    'weight': None,
+    'rind-color': None,
+    'locale': None
+    },
+'Crane': {
+    'price': 2.50,
+    'seedless': False,
+    'flesh-color': None,
+    'weight': None,
+    'rind-color': None,
+    'locale': None
+    },
+'Casaba': {
+    'price': 2.50,
+    'seedless': False,
+    'flesh-color': None,
+    'weight': None,
+    'rind-color': None,
+    'locale': None
+    },
+'Cantaloupe': {
+    'price': 0.99,
+    'sedless': False,
+    'flesh-color': None,
+    'weight': None,
+    'rind-color': None,
+    'locale': None
+    }
 }
